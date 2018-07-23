6efac1e3656881bb9b3087072f6a04a6919488d2
A.J <xwlrbh@sina.cn>
2018-07-23 10:28:26 +0800

    Update uploadPhoto.php

--- php/uploadPhoto.php
+++ php/uploadPhoto.php
@@ -1,1 +1,8 @@
+<?php
+//为了方便阅读，这里仅仅是一个图片文件上传的最简单的示例，实际使用请另行编写相关代码
 
+move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" . $_FILES["file"]["name"]);
+
+//输出图片文件所在的地址，这里假设存放在“php/upload”目录下面，图片文件的地址最好输出的是完整路径，如：echo "http://localhost/php/upload/" . $_FILES["file"]["name"];
+echo "php/upload/" . $_FILES["file"]["name"];
+?>
